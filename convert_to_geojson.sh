0f25463d03e1cdce3f5ca403deb98452b38dbd9f
Olivier Leroy <leroy.oli@gmail.com>
2023-08-03 08:35:24 -0400

    update convert

--- convert_to_geojson.sh
+++ convert_to_geojson.sh
@@ -1,14 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 # shellcheck source=streetcred.sh
 # credential are store in an other file: streetcred.sh
 # it takes an argument: name of the table with schema
-# ex ./convert_to_geojson.sh staging.vt_test 
+# ex ./convert_to_geojson.sh staging.vt_test
 
-MY_PATH="/Users/olivierleroy/data_swamp/tiles"
+set -e
+
+MY_PATH="$HOME/data_swamp/tr"
 
 source streetcred.sh
 
 echo "Working on $1"
+echo "Here: $MY_PATH/$1"
 ogr2ogr -f "GeoJSONSeq" $MY_PATH/"$1" \
         PG:"host=$HOST dbname=data user=$PGUSER password=$PGPWD port=5432" "$1" \
         -t_srs EPSG:4326 
